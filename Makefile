0f8db0e5d38437c6a1af6a5934058ba059e6113e
Liran Ben Abu <liran.benabu@run.ai>
2024-06-16 18:44:28 +0300

    adding ReadMe and install Kwok by bash script

--- Makefile
+++ Makefile
@@ -51,7 +51,7 @@ endif
 OPERATOR_SDK_VERSION ?= v1.34.2
 
 # Image URL to use all building/pushing image targets
-IMG ?= runai.jfrog.io/kwok-operator/kwok-controller:${VERSION}
+IMG ?= docker.io/runaidevops/kwok-operator:${VERSION}
 # ENVTEST_K8S_VERSION refers to the version of kubebuilder assets to be downloaded by envtest binary.
 ENVTEST_K8S_VERSION = 1.28.3
 
--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -60,7 +60,7 @@ var _ = Describe("controller", Ordered, func() {
 			var err error
 
 			// projectimage stores the name of the image used in the example
-			var projectimage = "example.com/kwok-operator:v0.0.1"
+			var projectimage = "example.com/kwok-operator:0.0.1"
 
 			By("building the manager(Operator) image")
 			cmd := exec.Command("make", "docker-build", fmt.Sprintf("IMG=%s", projectimage))
