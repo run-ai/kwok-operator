59dc3b0ecb556d056a0a0c1d00593218d551b2e4
Liran Ben Abu <liran.benabu@run.ai>
2024-06-26 22:17:32 +0300

    support deploymentPool

--- internal/controller/deploymentpool_controller.go
+++ internal/controller/deploymentpool_controller.go
@@ -18,9 +18,10 @@ package controller
 
 import (
 	"context"
-	"strings"
+	"time"
 
 	"k8s.io/apimachinery/pkg/runtime"
+
 	//"k8s.io/client-go/tools/clientcmd/api"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -94,7 +95,7 @@ func (r *DeploymentPoolReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 		log.Info("Adding Finalizer for the DeploymentPool")
 		err = r.addFinalizer(ctx, deploymentPool)
 		if err != nil {
-			log.Error(err, "unable to update DeploymentPool")
+			log.Error(err, "unable to add Finalizer for the DeploymentPool")
 			return ctrl.Result{}, err
 		}
 		return ctrl.Result{}, nil
@@ -102,24 +103,84 @@ func (r *DeploymentPoolReconciler) Reconcile(ctx context.Context, req ctrl.Reque
 
 	// Get Deployment in the cluster with owner reference to the DeploymentPool
 	deployment, err := r.getDeployment(ctx, deploymentPool)
-	log.Info("Deployment is here", "Deployment", deployment)
 	if err != nil {
-		log.Info("Deployment not found", "DeploymentPool", deploymentPool)
-		log.Error(err, "unable to fetch Deployment name", "DeploymentPool", deployment)
+		//log.Error(err, "unable to get Deployment", deploymentPool)
 		return ctrl.Result{}, err
 	}
 	// Create Deployment if it does not exist
 	if deployment == nil {
-		log.Info("Creating Deployment", "DeploymentPool", deploymentPool)
 		err = r.createDeployment(ctx, deploymentPool)
 		if err != nil {
-			log.Error(err, "unable to create Deployment", "DeploymentPool", deploymentPool)
 			return ctrl.Result{}, err
+		}
+		return ctrl.Result{Requeue: true}, nil
+	}
+	// update DeploymentPool status to condition true
+	err = r.statusConditionController(ctx, deploymentPool, metav1.Condition{
+		Type:    "Available",
+		Status:  metav1.ConditionTrue,
+		Reason:  "DeploymentPoolReconciled",
+		Message: "DeploymentPool reconciled successfully",
+	})
+	if err != nil {
+		log.Error(err, "unable to update DeploymentPool status")
+		return ctrl.Result{Requeue: true}, nil
+	}
+	// update status of the deployment pool
+
+	if deploymentPool.Status.ObservedGeneration != deploymentPool.Generation {
+		log.Info("DeploymentPool generation has changed, requeuing")
+		err = r.Get(ctx, req.NamespacedName, deploymentPool)
+		if err != nil {
+			log.Error(err, "unable to fetch DeploymentPool")
+			return ctrl.Result{}, err
+		}
+		err = r.statusConditionController(ctx, deploymentPool, metav1.Condition{
+			Type:    "Available",
+			Status:  metav1.ConditionFalse,
+			Reason:  "DeploymentPoolReconciling",
+			Message: "Updating DeploymentPool",
+		})
+		log.Info("Updating DeploymentPool")
+		if err != nil {
+			log.Error(err, "unable to update DeploymentPool status")
+			return ctrl.Result{}, nil
+		}
+		err = r.updateDeployment(ctx, deploymentPool)
+		if err != nil {
+			log.Error(err, "unable to update Deployment")
+			return ctrl.Result{}, nil
+		}
+
+		return ctrl.Result{}, nil
+	}
+	if !deploymentPool.DeletionTimestamp.IsZero() {
+		log.Info("Deleting DeploymentPool")
+		err = r.statusConditionController(ctx, deploymentPool, metav1.Condition{
+			Type:    "Available",
+			Status:  metav1.ConditionFalse,
+			Reason:  "Deleting",
+			Message: "Deleting the deploymentPool",
+		})
+		if err != nil {
+			log.Error(err, "unable to update DeploymentPool status")
+			return ctrl.Result{}, nil
+		}
+		err = r.Delete(ctx, deployment)
+		if err != nil {
+			log.Error(err, "unable to delete Deployment")
+			return ctrl.Result{}, nil
+		}
+		err = r.deleteFinalizer(ctx, deploymentPool)
+		if err != nil {
+			log.Error(err, "unable to delete Finalizer")
+			return ctrl.Result{}, nil
 		}
 		return ctrl.Result{}, nil
 	}
 
-	return ctrl.Result{}, nil
+	log.Info("Reconciliation completed successfully")
+	return ctrl.Result{RequeueAfter: time.Duration(60 * time.Second)}, nil
 }
 
 func (r *DeploymentPoolReconciler) statusConditionController(ctx context.Context, deploymentPool *kwoksigsv1beta1.DeploymentPool, condition metav1.Condition) error {
@@ -138,35 +199,70 @@ func (r *DeploymentPoolReconciler) deleteFinalizer(ctx context.Context, deployme
 }
 
 func (r *DeploymentPoolReconciler) getDeployment(ctx context.Context, deploymentPool *kwoksigsv1beta1.DeploymentPool) (*appsv1.Deployment, error) {
-	deployment := &appsv1.DeploymentList{}
-	err := r.List(ctx, deployment, client.InNamespace(deploymentPool.Namespace), client.MatchingLabels{controllerLabel: deploymentPool.Name})
-	if err != nil && strings.Contains(err.Error(), "does not exist") {
-		return nil, nil
-	} else if err != nil {
+	// get deplyment with the name of deploymentPool name
+	deployment := &appsv1.Deployment{}
+	err := r.Get(ctx, client.ObjectKey{
+		Namespace: deploymentPool.Namespace,
+		Name:      deploymentPool.Name,
+	}, deployment)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			return nil, nil
+		}
 		return nil, err
 	}
-	if len(deployment.Items) == 0 {
-		return nil, nil
+	return deployment, nil
+}
+
+// update deployment
+func (r *DeploymentPoolReconciler) updateDeployment(ctx context.Context, deploymentPool *kwoksigsv1beta1.DeploymentPool) error {
+	// get the deployment spec from the cluster
+	deployment, err := r.getDeployment(ctx, deploymentPool)
+	log.Log.Info("Updating Deployment", "Deployment", deployment)
+	if err != nil {
+		return err
 	}
-	return &deployment.Items[0], nil
+	deployment.Spec.Replicas = deploymentPool.Spec.DeploymentTemplate.Spec.Replicas
+	deployment.Spec.Template.Spec.Containers = deploymentPool.Spec.DeploymentTemplate.Spec.Template.Spec.Containers
+	err = r.Update(ctx, deployment)
+	if err != nil {
+		return err
+	}
+	err = r.updateObservedGeneration(ctx, deploymentPool)
+	if err != nil {
+		return err
+	}
+	return nil
+
 }
 
 // create deployment
 func (r *DeploymentPoolReconciler) createDeployment(ctx context.Context, deploymentPool *kwoksigsv1beta1.DeploymentPool) error {
+	appendSelector := deploymentPool.Spec.DeploymentTemplate.Spec.Selector
+	appendSelector.MatchLabels[controllerLabel] = deploymentPool.Name
+
+	overrideLabels := deploymentPool.Spec.DeploymentTemplate.Spec.Template.Labels
+	if overrideLabels == nil {
+		overrideLabels = map[string]string{
+			controllerLabel: deploymentPool.Name,
+		}
+	} else {
+		overrideLabels[controllerLabel] = deploymentPool.Name
+	}
+
 	deploymentToleration := deploymentPool.Spec.DeploymentTemplate.Spec.Template.Spec.Tolerations
 	if deploymentToleration == nil {
-		log.Log.Info("Pod tolerations is nil")
 		deploymentToleration = make([]corev1.Toleration, 0)
 	}
+	
 	deploymentToleration = append(deploymentToleration, corev1.Toleration{
 		Key:      controllerAnnotation,
-		Value:    fakeString,
 		Operator: corev1.TolerationOpExists,
 		Effect:   corev1.TaintEffectNoSchedule,
 	})
+
 	deployment := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
-			GenerateName: deploymentPool.Name + "-",
 			OwnerReferences: []metav1.OwnerReference{
 				*metav1.NewControllerRef(deploymentPool, kwoksigsv1beta1.GroupVersion.WithKind("DeploymentPool")),
 			},
@@ -175,9 +271,11 @@ func (r *DeploymentPoolReconciler) createDeployment(ctx context.Context, deploym
 		},
 		Spec: deploymentPool.Spec.DeploymentTemplate.Spec,
 	}
+
+	deployment.Spec.Template.ObjectMeta.Labels = overrideLabels
+	deployment.Spec.Selector = appendSelector
 	deployment.Spec.Template.Spec.Tolerations = deploymentToleration
 
-	log.Log.Info("present entire object", "DeploymentPool", deploymentPool)
 	err := r.Create(ctx, deployment)
 	if err != nil {
 		return err
