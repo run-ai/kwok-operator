8dbf9af64177ec9a3fc28c4284dd88b358224cec
Liran Ben Abu <liran.benabu@run.ai>
2024-07-01 10:48:31 +0300

    fixed comments

--- internal/controller/deploymentpool_controller.go
+++ internal/controller/deploymentpool_controller.go
@@ -46,17 +46,7 @@ type DeploymentPoolReconciler struct {
 //+kubebuilder:rbac:groups=kwok.sigs.run-ai.com,resources=deploymentpools/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=kwok.sigs.run-ai.com,resources=deploymentpools/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the DeploymentPool object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
 func (r *DeploymentPoolReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
 	log := log.FromContext(ctx)
 	log.Info("Reconciling DeploymentPool")
 	deploymentPool := &kwoksigsv1beta1.DeploymentPool{}
